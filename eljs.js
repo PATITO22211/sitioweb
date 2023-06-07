c43739d99de599d69a215827d1d2120cc7f77584
Patito <104477345+PATITO22211@users.noreply.github.com>
2023-06-06 18:53:24 -0600

    Update eljs.js

--- eljs.js
+++ eljs.js
@@ -1,6 +1,6 @@
 const typed = new Typed('.typed', {
 	strings: [
-		  '<i class="texto">Lee el título</i>',
+	'<i class="texto">lee el titulo</i>',
     '<i class="texto">¿si?</i>',
 		// '<i class="texto">¿te gustaría ser mi novia?</i>'
 		// '<i class="texto">Python</i>',
@@ -13,13 +13,13 @@ const typed = new Typed('.typed', {
 	],
 
 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 67, // Velocidad en mlisegundos para poner una letra,
-	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	typeSpeed: 100, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 2000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
 	backSpeed: 30, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
-	loop: false, // Repetir el array de strings
+	loop: true, // Repetir el array de strings
 	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
 	showCursor: true, // Mostrar cursor palpitanto
 	cursorChar: '|', // Caracter para el cursor
