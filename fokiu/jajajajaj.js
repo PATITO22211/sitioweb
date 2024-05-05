ea5ee4e12dfeb5e779deff84c27368026da3d43e
Patito <104477345+PATITO22211@users.noreply.github.com>
2024-05-05 00:26:35 -0600

    Update jajajajaj.js

--- fokiu/jajajajaj.js
+++ fokiu/jajajajaj.js
@@ -3,13 +3,15 @@ const typed = new Typed('.typed', {
 		'<i class="texto">México</i>',
 		'<i class="texto">Mid</i>',
 		'<i class="texto">@da_rio.patito</i>',
-		'<i class="texto">JGD</i>'
+		'<i class="texto">Al final siempre tiene que haber lluvia para que las flores puedan florecer.</i>',
+		'<i class="texto">JGHD</i>',
+		'<i class="texto">. . .</i>',
 	],
 
 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	typeSpeed: 79, // Velocidad en mlisegundos para poner una letra,
 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	backSpeed: 80, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
