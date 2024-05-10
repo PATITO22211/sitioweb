97c4d6be5043da71165999ae512981c9e442de7d
Patito <104477345+PATITO22211@users.noreply.github.com>
2024-05-09 22:09:27 -0600

    Update jajajajaj.js

--- fokiu/jajajajaj.js
+++ fokiu/jajajajaj.js
@@ -1,17 +1,24 @@
 const typed = new Typed('.typed', {
 	strings: [
-		'<i class="texto">México</i>',
-		'<i class="texto">Mid</i>',
+		'<i class="texto">the best website in the world</i>',
+		'<i class="texto">MID</i>',
 		'<i class="texto">@da_rio.patito</i>',
 		'<i class="texto">Siempre tiene que haber lluvia para que las flores puedan salir.</i>',
-		'<i class="texto">JGHD</i>',
+		// '<i class="texto">why are you here??</i>',
 		'<i class="texto">. . .</i>',
+		'<i class="texto">I think Im falling in love</i>',
+		"<i class='texto'>I think she's perfect</i>",
+		"<i class='texto'>but don't say anything...</i>",
+		"<i class='texto'>🤫</i>",
+
+
+
 	],
 
 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 79, // Velocidad en mlisegundos para poner una letra,
+	typeSpeed: 60, // Velocidad en mlisegundos para poner una letra,
 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 80, // Velocidad en milisegundos para borrrar una letra,
+	backSpeed: 15, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
