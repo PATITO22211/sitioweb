5a7eac412e30342e152ab966754a9adfbb498a2d
Patito <104477345+PATITO22211@users.noreply.github.com>
2024-06-16 22:11:50 -0600

    Update jajajajaj.js

--- fokiu/jajajajaj.js
+++ fokiu/jajajajaj.js
@@ -3,6 +3,7 @@ const typed = new Typed('.typed', {
 		'<i class="texto">the best website in the world</i>',
 		'<i class="texto">MID</i>',
 		'<i class="texto">@da_rio.patito</i>',
+	
 		'<i class="texto">9268</i>',
 		"<i class='texto'>I make the best websites</i>",
 		'<i class="texto">todos enciendan un incienso no por las vibras, es por que apestan a hipocresía</i>',
@@ -33,5 +34,45 @@ const typed = new Typed('.typed', {
 	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
 	showCursor: true, // Mostrar cursor palpitanto
 	cursorChar: '🔫', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+const typed2 = new Typed('.typed2', {
+	strings: [
+		'<i class="texto">Imagino, Diseño y Programo los mejores sitios web que existen</i>',
+		'<i class="texto">+52</i>',
+		'<i class="texto">😵‍💫</i>',
+		'<i class="texto">Im going to be a fucking money maker</i>',
+		"<i class='texto'>Dario monito</i>",
+		'<i class="texto">Business ideas</i>',
+		'<i class="texto">ⅩⅣ</i>',
+		'<i class="texto">school does not teach you how to generate money</i>',
+		"<i class='texto'>DXRG</i>",
+		"<i class='texto'>→ follow me in ig ←</i>",
+		"<i class='texto'>🤫</i>",
+		"<i class='texto'>I'm not crazy just nobody understands me</i>",
+		// "<i class='texto'>waiting for love</i>",
+		"<i class='texto'>😎🔥</i>",
+		"<i class='texto'>🧨</i>",
+		"<i class='texto'>I'm going to be the money man</i>",
+		"<i class='texto'>2009</i>",
+		"<i class='texto'>MAKING MONEY SINCE 2023</i>",
+
+		"<i class='texto'>₯←</i>",
+
+
+	],
+
+	// stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 1, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 1300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 5, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1000, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
 	contentType: 'html', // 'html' o 'null' para texto sin formato
 });
